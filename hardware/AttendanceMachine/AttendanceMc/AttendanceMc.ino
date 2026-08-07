c3a99a4426343b1d160620407162878777f64f8d
sandeep8558 <sandeep198558@gmail.com>
2026-08-07 22:49:12 +0530

    perf(hardware): set BearSSL RX buffer size to 3072 bytes (3KB) for optimal SSL handshake without heap exhaustion

--- hardware/AttendanceMachine/AttendanceMc/AttendanceMc.ino
+++ hardware/AttendanceMachine/AttendanceMc/AttendanceMc.ino
@@ -1294,9 +1294,9 @@ SyncResult sendDataToServerParams(String tId, String tMs, String d, String t) {
   if (isHttps) {
     BearSSL::WiFiClientSecure client;
     client.setInsecure();
-    client.setBufferSizes(2048, 512);
-    client.setTimeout(5000);
-    http.setTimeout(5000);
+    client.setBufferSizes(3072, 512);
+    client.setTimeout(6000);
+    http.setTimeout(6000);
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
     http.setUserAgent("ESP8266-AttendanceMachine");
 
